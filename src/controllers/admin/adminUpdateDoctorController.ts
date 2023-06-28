57a84dd7441430dbfca30f05b88ab832480cc01d
ChriMarc71 <chrimarc71@gmail.com>
2023-06-28 15:46:45 +0200

    formatted adminControllers

--- src/controllers/admin/adminUpdateDoctorController.ts
+++ src/controllers/admin/adminUpdateDoctorController.ts
@@ -1,28 +1,28 @@
 import Prisma from "../../database/connection";
 
 const updateDoctor = async (req: any, res: any) => {
-    const id: number = req.body.id;
-    const toChange: string = req.body.toChange;
-    const data: any = req.body.data;
+  const id: number = req.body.id;
+  const toChange: string = req.body.toChange;
+  const data: any = req.body.data;
 
-    if (
-        typeof id === "undefined" ||
-        typeof toChange === "undefined" ||
-        typeof data === "undefined"
-    ) {
-        res.status(400).send("undefined field/s!");
-    }
+  if (
+    typeof id === "undefined" ||
+    typeof toChange === "undefined" ||
+    typeof data === "undefined"
+  ) {
+    res.status(400).send("undefined field/s!");
+  }
 
-    await Prisma.doctors.update({
-        where: {
-            Id: id,
-        },
-        data: {
-            [toChange]: data,
-        },
-    });
+  await Prisma.doctors.update({
+    where: {
+      Id: id,
+    },
+    data: {
+      [toChange]: data,
+    },
+  });
 
-    res.send("Updated");
+  res.send("Updated");
 };
 
 export default updateDoctor;
