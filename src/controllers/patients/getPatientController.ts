5f470c18342bff1184f459480ab02660555b05d7
SimoneRimedio <rimediosimone1@gmail.com>
2023-06-28 13:03:10 +0200

    patients controller added

---
+++ src/controllers/patients/getPatientController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getPatients = async (req: Request, res: Response): Promise<void> => {
+  let id: number | undefined = req.body.id;
+  let patients;
+  if (typeof id !== "undefined") {
+    patients = await prisma.patients.findUnique({
+      where: {
+        Id: id,
+      },
+    });
+  } else {
+    patients = await prisma.patients.findMany({});
+  }
+
+  res.send(patients);
+};
+
+export default getPatients;
---
+++ src/controllers/patients/updatePatientController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const updatePatients = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const id: number = req.body.id;
+  const toChange: string = req.body.toChange;
+  const data: string = req.body.data;
+  if (
+    typeof id === "undefined" ||
+    typeof toChange === "undefined" ||
+    typeof data === "undefined"
+  ) {
+    res.status(400).send("undefined field/s!");
+    return;
+  }
+  prisma.patients.update({
+    where: {
+      Id: id,
+    },
+    data: "{" + toChange + ":" + data + "}",
+  });
+  res.send("Updated");
+};
+
+export default updatePatients;
