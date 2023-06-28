61fe707b2668631318fd2ab6f9016b88e063e72c
ChriMarc71 <chrimarc71@gmail.com>
2023-06-28 14:27:26 +0200

    addedd authLoginController.ts

---
+++ src/controllers/auth/authLoginController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import Prisma from "../../database/connection";
+import jwt from "jsonwebtoken";
+
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const founded = await Prisma.patients.findMany({
+    select: {
+      Id: true,
+    },
+    where: {
+      Username: username,
+      Password: password,
+      IsEnable: true,
+    },
+  });
+  if (founded.length != 0) {
+    const token = jwt.sign(
+      { Id: founded[0].Id },
+      "AlgernonAlgernonAlgernonAlgernon",
+      {
+        expiresIn: "4h",
+        algorithm: "HS256",
+      }
+    );
+    res.json({
+      Token: token,
+    });
+  } else {
+    res.send(
+      "Credentials invalid or account not enabled, check emails to confirm the account!"
+    );
+  }
+};
+
+export default login;
